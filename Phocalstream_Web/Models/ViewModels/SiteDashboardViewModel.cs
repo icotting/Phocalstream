18387662740ff99f63d7834f2b36f99a2ffefe25
Zach <zm.christensen@cox.net>
2014-09-24 16:36:19 -0500

    Implemented the initial view model and page.

---
+++ Phocalstream_Web/Models/ViewModels/SiteDashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Phocalstream_Web.Models.ViewModels
+{
+    public class SiteDashboardViewModel
+    {
+        public CollectionViewModel CollectionViewModel { get; set; }
+
+
+    }
+}
