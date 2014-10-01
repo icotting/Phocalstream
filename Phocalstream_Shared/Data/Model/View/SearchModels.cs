2582a85f65995874d694ec5a497c5d25bb56195d
Zach <zm.christensen@cox.net>
2014-10-01 09:23:16 -0500

    Fixed the naming method in the search model and fixed the knockout binding to show 1 photo or x photos depending on search result count.

--- Phocalstream_Shared/Data/Model/View/SearchModels.cs
+++ Phocalstream_Shared/Data/Model/View/SearchModels.cs
@@ -43,16 +43,17 @@ namespace Phocalstream_Shared.Data.Model.View
             StringBuilder name = new StringBuilder(); 
             name.Append("Showing photos ");
 
+            bool found = false;
             if(!String.IsNullOrWhiteSpace(Sites) && !Sites.Equals("undefined"))
             {
                 string[] siteSplit = Sites.Split(',');
                 if (siteSplit.Length == 1)
                 {
-                    name.Append("from " + siteSplit[0] + " ");
+                    name.Append("from " + siteSplit[0] + "");
                 }
                 else if (siteSplit.Length == 2)
                 {
-                    name.Append("from " + siteSplit[0] + " or " + siteSplit[1] + " ");
+                    name.Append("from " + siteSplit[0] + " or " + siteSplit[1] + "");
                 }
                 else
                 {
@@ -61,34 +62,101 @@ namespace Phocalstream_Shared.Data.Model.View
                     {
                         name.Append(siteSplit[i] + ", ");
                     }
-                    name.Append(" or " + siteSplit[siteSplit.Length - 1] + " ");
+                    name.Append(" or " + siteSplit[siteSplit.Length - 1] + "");
                 }
+                found = true;
             }
 
             if (!String.IsNullOrWhiteSpace(Tags) && !Tags.Equals("undefined"))
             {
-                name.Append("tagged with " + String.Join(", ", Tags.Split(',')) + " ");
+                if (found)
+                {
+                    name.Append(", and ");
+                }
+
+                string[] tagSplit = Tags.Split(',');
+                if (tagSplit.Length == 1)
+                {
+                    name.Append("tagged with " + tagSplit[0] + "");
+                }
+                else if (tagSplit.Length == 2)
+                {
+                    name.Append("tagged with " + tagSplit[0] + " or " + tagSplit[1] + "");
+                }
+                else
+                {
+                    name.Append("tagged with ");
+                    for (int i = 0; i < tagSplit.Length - 1; i++)
+                    {
+                        name.Append(tagSplit[i] + ", ");
+                    }
+                    name.Append(" or " + tagSplit[tagSplit.Length - 1] + "");
+                }
+                found = true;
             }
 
             //MONTHS
             List<string> monthNames = new List<string>();
             if (!String.IsNullOrWhiteSpace(this.Months))
             {
+                if (found)
+                {
+                    name.Append(", and ");
+                }
+
                 string[] months = this.Months.Split(',');
                 foreach (var m in months)
                 {
                     monthNames.Add(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(Convert.ToInt16(m)));
                 }
 
+                if (monthNames.Count == 1)
+                {
+                    name.Append("taken during the month of " + monthNames[0] + "");
+                }
+                else if (monthNames.Count == 2)
+                {
+                    name.Append("taken during the months of " + monthNames[0] + " or " + monthNames[1] + "");
+                }
+                else
+                {
+                    name.Append("taken during the months of ");
+                    for (int i = 0; i < monthNames.Count - 1; i++)
+                    {
+                        name.Append(monthNames[i] + ", ");
+                    }
+                    name.Append(" or " + monthNames[monthNames.Count - 1] + "");
+                }
+                found = true;
             }
-            if(monthNames.Count != 0)
-            {
-                name.Append("taken during " + String.Join(", ", monthNames.ToArray()) + " ");
-            }
+
 
             if (!String.IsNullOrWhiteSpace(Dates) && !Dates.Equals("undefined"))
             {
-                name.Append("taken on " + String.Join(", ", Dates.Split(',')) + " ");
+                if (found)
+                {
+                    name.Append(", and ");
+                }
+
+                string[] dateSplit = Dates.Split(',');
+                if (dateSplit.Length == 1)
+                {
+                    name.Append("taken on " + dateSplit[0] + "");
+                }
+                else if (dateSplit.Length == 2)
+                {
+                    name.Append("taken on " + dateSplit[0] + " or " + dateSplit[1] + "");
+                }
+                else
+                {
+                    name.Append("taken on ");
+                    for (int i = 0; i < dateSplit.Length - 1; i++)
+                    {
+                        name.Append(dateSplit[i] + ", ");
+                    }
+                    name.Append(" or " + dateSplit[dateSplit.Length - 1] + "");
+                }
+                found = true;
             }
 
 
@@ -96,15 +164,35 @@ namespace Phocalstream_Shared.Data.Model.View
             List<string> hourNames = new List<string>();
             if (!String.IsNullOrWhiteSpace(this.Hours))
             {
+                if (found)
+                {
+                    name.Append(", and ");
+                }
+             
                 string[] hours = this.Hours.Split(',');
                 foreach (var h in hours)
                 {
                     hourNames.Add(h + "00");
                 }
-            }
-            if(hourNames.Count != 0)
-            {
-                name.Append("during the hours of " + String.Join(", ", hourNames.ToArray()) + ".");
+
+                if (hourNames.Count == 1)
+                {
+                    name.Append("taken during the hour of " + hourNames[0] + "");
+                }
+                else if (hourNames.Count == 2)
+                {
+                    name.Append("taken during the hours of " + hourNames[0] + " or " + hourNames[1] + "");
+                }
+                else
+                {
+                    name.Append("taken during the hours of ");
+                    for (int i = 0; i < hourNames.Count - 1; i++)
+                    {
+                        name.Append(hourNames[i] + ", ");
+                    }
+                    name.Append(" or " + hourNames[hourNames.Count - 1] + "");
+                }
+                found = true;
             }
 
             return name.ToString();
