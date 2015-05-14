8d2f81fe4b18d2bbb3990bb1706d43b9620a41c4
Zach <zm.christensen@cox.net>
2015-05-14 13:24:07 -0500

    Cleaned up the account controller and removed pivotviewer endpoint no longer needed.

--- Phocalstream_Shared/Data/Model/View/UserCollectionModels.cs
+++ Phocalstream_Shared/Data/Model/View/UserCollectionModels.cs
@@ -9,22 +9,6 @@ using System.Threading.Tasks;
 
 namespace Phocalstream_Shared.Data.Model.View
 {
-    public class UserDefinedCollection
-    {
-        public string CollectionName { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
-        public DateTime First { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
-        public DateTime Last { get; set; }
-        
-        [DisplayFormat(DataFormatString = "{0:#,#}")]
-        public int PhotoCount { get; set; }
-        public string CollectionUrl { get; set; }
-        public UserCollectionList UserCollections { get; set; }
-    }
-
     public class UserCollectionList
     {
         public User User { get; set; }
